4bdd176462bb24a8191bed392e46851d12786899
kootsoop <peter@kootsoop.com>
2024-10-13 12:54:55 -0400

    Make speed test take more time via loop.

--- src/main/java/com/arexperts/TestReadSpeed.java
+++ src/main/java/com/arexperts/TestReadSpeed.java
@@ -13,7 +13,7 @@ public class TestReadSpeed {
 
     private static final double startTime = System.nanoTime();
 
-    public static void main(String[] args) {
+    private static void doRead() {
         int row = 0;        
         File file = new File("test.csv");
         ArticleIndex articles = new ArticleIndex(5);
@@ -24,7 +24,8 @@ public class TestReadSpeed {
 
                 String articleToAdd = record.get(6).trim();        
                 articles.addArticle(articleToAdd, "row" + row++);
-            } 
+            }
+            System.out.println(articles.findMatch("It is undoubtedly an opulent and striking pictorial spectacle that Cecil B. DeMille has wrought from Wilson Barrett's famous old stage work, \"\"The Sign of the Cross.\"\" It was offered last night at the Rialto before a brilliant audience, which revealed no little interest in its variety of handsome scenes, its battling gladiators, its orgies in Nero's court, its chanting Christians, its music and its screams.Throughout this really mammoth production the fine DeMillean hand is noticeable. Where there was a chance to touch up episodes it has been done. It is as though Nero were living in the twentieth century, with some of the lines and the squabbling in the Rome arena for places to see the big bill, which includes many combats between Nero's own subjects and scores of Christians and others marching to their deaths. The hungry lions rush up stone steps, eager to get to their human prey, and, just before that, a dying man is supposed to have his head trampled on by an elephant.The principal rôles are all well played, even though they are more or less in the modern manner. But the outstanding histrionic achievement comes from Charles Laughton, who shoulders the responsibility for Nero. He is a petulent Nero, a man who has no thought for other than himself, and when he is asked to grant the life of")[1]); 
         }
         catch(IOException ex) {
             System.err.println("File '" + file.getName() +  "' caught exception: " + ex.getLocalizedMessage());
@@ -32,6 +33,11 @@ public class TestReadSpeed {
         catch(OutOfMemoryError exMemoryError)
         {
             System.err.println("File '" + file.getName() +  "' caught exception: " + exMemoryError.getLocalizedMessage());
+        }
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            doRead();
         }
         System.out.println("Total elapsed time : " + getElapsedTime());               
     }
